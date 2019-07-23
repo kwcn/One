e09528b1eafc878f2c4de6e3fdafae2861c969e4
kangwei <kangwei@gwm.cn>
2019-07-23 13:54:40 +0800

    add base repo.

---
+++ app/src/main/java/com/kw/one/repo/base/NoParamsRepo.java
@@ -0,0 +1,17 @@
+package com.kw.one.repo.base;
+
+import androidx.annotation.WorkerThread;
+import androidx.lifecycle.LiveData;
+
+/**
+ * @author Kang Wei
+ * @date 2019/7/23
+ */
+public abstract class NoParamsRepo<T> {
+    public abstract LiveData<T> getLiveData();
+
+    @WorkerThread
+    public abstract T getData();
+
+    public abstract void reload();
+}
---
+++ app/src/main/java/com/kw/one/repo/base/Repo.java
@@ -0,0 +1,21 @@
+package com.kw.one.repo.base;
+
+import androidx.annotation.WorkerThread;
+import androidx.lifecycle.LiveData;
+
+/**
+ * 数据模型
+ *
+ * @author Kang Wei
+ * @date 2019/7/23
+ */
+public abstract class Repo<R, T> {
+    // 适用于ui controller的异步数据
+    public abstract LiveData<T> getLiveData(R r);
+
+    // 适用于非ui的数据访问，且是同步访问
+    @WorkerThread
+    public abstract T getData(R r);
+
+    public abstract void reload();
+}
